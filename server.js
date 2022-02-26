98965ad2e4bd45472714af101688e0ca82fd40b0
Kyleah Justice <kikij432@gmail.com>
2022-02-25 20:42:00 -0700

    finished the get path, actually understood it. yay!

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.post('/api/notes', (req, res) => {
     }
 })
 
+app.get('/', (req,res) => {
+    res,sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
 
 
 
