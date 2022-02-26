b8fede9a247c2c42d6d7c162b794258992345715
Kyleah Justice <kikij432@gmail.com>
2022-02-25 20:28:29 -0700

    A little iffy on my create new function, but google is a beautiful thing.

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const path = require('path');
 const express = require('express');
 const { notes } = require('./Develop/db/db.json');
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join('public')));
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note)
+    
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({notes: notesArray }, null, 2)
+    );
+
+    return body;
+
+}
 
 
 
