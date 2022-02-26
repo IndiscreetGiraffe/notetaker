13c5515d25e93b16c68a2eebc6f95161defdffe0
Kyleah Justice <kikij432@gmail.com>
2022-02-25 20:16:08 -0700

    added required paths, starting on connecting note in db.json

---
+++ server.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { notes } = require('./Develop/db/db.json');
+
+const PORT = 3000
+const app = express();
+
+
+
+
